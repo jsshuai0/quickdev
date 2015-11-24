5b771a0aaa16ccc381d8b0335476a01eb333631a
Administrator <Administrator@www.zh.cao.com>
2015-11-24 17:48:38 +0800

    RequestHelper

--- QuickDEV/src/com/anthole/quickdev/ui/RequestHelper/IRequestHelper.java
+++
@@ -1,9 +0,0 @@
-package com.anthole.quickdev.ui.RequestHelper;
-
-import com.anthole.quickdev.http.ResponseHandlerInterface;
-
-public interface IRequestHelper {
-	
-	public ResponseHandlerInterface getResponseHandler();
-	
-}
